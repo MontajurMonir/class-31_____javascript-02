441defc0a56414c11aabe84832a8ebec60317444
Montajur Rahman Monir <montajurrahmanmonir@gmail.com>
2025-06-17 20:07:14 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,79 @@
+// Namata 1 X 5 = 5
+
+console.log(1 " "+ "X" 5 = 5);
+
+
+
+
+
+
+
+// let x = 20
+// let y = 30
+// console.log(x+y)
+
+
+// // let teams = "monir,rafi,forhad,fuyad"
+
+
+// // Array 
+// let teams = ["monir","rafi","forhad","fuyad",504]
+
+// console.log(teams); 
+
+// console.log(teams[0]);
+// console.log(teams[2]);
+// // Array End 
+
+
+// // Object 
+// let monir = {
+//     name: "Montajur",
+//     age: 23, 
+// }
+// console.log(monir);
+
+// console.log(monir.name);
+// console.log(monir.age);
+
+
+// let {name,age} = {
+//     name: "Montajur",
+//     age: 23,
+// }
+
+// console.log(name);
+// console.log(age);
+
+
+// let info = [
+//     {
+//         name:"Montajur Rahman ",
+//         age: 23
+//     },
+
+//     {
+//         name:"Forhad",
+//         age: 25
+//     },
+
+//     {
+//         name:"Fuyad",
+//         age:21
+//     },
+
+//     {
+//         name:"Rafi",
+//         age:24
+//     }
+// ]
+// console.log(info);
+
+// console.log(info[2]);
+// console.log(info[0].name);
+
+
+
+
+
+
